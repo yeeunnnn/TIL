950730e54e9e0ac25ff696f45a67b55c83c7c470
yeeunnnn <133733528+yeeunnnn@users.noreply.github.com>
2025-05-28 22:48:57 +0900

    setting

---
+++ til/src/main/java/com/til/service/MainService.java
@@ -0,0 +1,5 @@
+package com.til.service;
+
+public class MainService {
+
+}
---
+++ til/src/test/java/com/til/project/TilApplicationTests.java
@@ -0,0 +1,13 @@
+package com.til.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TilApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
